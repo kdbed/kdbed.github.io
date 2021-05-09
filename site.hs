12a3d6da59b5133b96d77c34742ee8c2ed6e4319
kdbed <kbednar3@kent.edu>
2021-05-08 22:56:52 -0400

    commit

--- site.hs
+++ site.hs
@@ -69,19 +69,19 @@ postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
 
-pandocMathCompiler :: Compiler (Item String)
-pandocMathCompiler =
-  let
-    mathExtensions =
-      [ Ext_tex_math_dollars
-      , Ext_tex_math_double_backslash
-      , Ext_latex_macros
-      ]
-    defaultExtensions = writerExtensions defaultHakyllWriterOptions
-    newExtensions = foldr enableExtension defaultExtensions mathExtensions
-    writerOptions =
-      defaultHakyllWriterOptions
-      { writerExtensions = newExtensions
-      , writerHTMLMathMethod = MathJax ""
-      }
-  in pandocCompilerWith defaultHakyllReaderOptions writerOptions
+//pandocMathCompiler :: Compiler (Item String)
+//pandocMathCompiler =
+//  let
+//    mathExtensions =
+//      [ Ext_tex_math_dollars
+//      , Ext_tex_math_double_backslash
+//      , Ext_latex_macros
+//      ]
+//    defaultExtensions = writerExtensions defaultHakyllWriterOptions
+//    newExtensions = foldr enableExtension defaultExtensions mathExtensions
+//    writerOptions =
+//      defaultHakyllWriterOptions
+//      { writerExtensions = newExtensions
+//      , writerHTMLMathMethod = MathJax ""
+//      }
+//  in pandocCompilerWith defaultHakyllReaderOptions writerOptions
